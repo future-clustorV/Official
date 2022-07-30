26738a83d0ed73e72d5eaaecaa84fffb815c0fe1
henohenon <takeru.hawk.son@gmail.com>
2022-07-30 16:38:05 +0900

    ニュースを修正。

--- news/eachNews.js
+++ news/eachNews.js
@@ -10,6 +10,5 @@ window.addEventListener('DOMContentLoaded', function() {
 });
 
 const newsData = {
-    '#first':{newsTitle:'一期生初配信', imagePath:'../images/newsImage/first.png', newsText:'初配信らしいわよ\nわわ'},
-    '#hello':{newsTitle:'ご挨拶', imagePath:'../images/group-logo.png', newsText:'なぜかこんなところまで来させてもらえたのでせいぜいあがくぜ'}
+    '#first':{newsTitle:'一期生初配信', imagePath:'../images/newsImage/first.png', newsText:'ついにミライノムレより4人のVtuberたちが生まれました！\n阿古町孤夢\nお喋り大好き孤。のんびり雑談系VTuber\n猫山ノラ\nコーヒーや温泉みたいにのんびりできる配信をしていきます。野良猫系VTuber\n月ノ宮レイン\nゲームとお絵描きが大好きな駅員系Vtuber(？)\nはんぺん王子inukai\n涙も全部はんぺんで拭ってあげるからあなたの隣でしょうもなく生きる！天ぷら犬Vtuber\n関わってくれた全ての人に感謝です...😌今後とも応援のほどどうぞよろしくお願いいたします。'},
 };
